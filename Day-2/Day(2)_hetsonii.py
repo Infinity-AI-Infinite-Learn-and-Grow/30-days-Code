f6fb1476326a4d4c682d91754eda950a3e889abe
hetsonii <21cs068@charusat.edu.in>
2023-02-06 21:17:50 +0530

    Day2

---
+++ Day-2/Day(2)_hetsonii.py
@@ -0,0 +1,17 @@
+#Declaring the Variables
+
+FirstName = input("Enter Your First Name: ")
+LastName = input("Enter Your Last Name: ")
+Contry = input("Enter Your Contry Name: ")
+City = input("What city do you live in?: ")
+Age = int(input("What is Your Age?: "))
+DOB = '21/06/2004'
+
+#Checking Data Types using the type() built-in function.
+
+print('Data type of FirstName is {}'.format(type(FirstName)))
+print('Data type of LastName is {}' .format(type(LastName)))
+print('Data type of Contry is {}'.format(type(Contry)))
+print('Data type of City {}' .format(type(City)))
+print('Data type of Age {}' .format(type(Age)))
+print('Data type of DOB {}' .format(type(DOB)))
