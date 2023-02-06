7bfdf40a481a159a37abd255f2ba4f4df9a6e9ef
jayant-01 <01agarwal.jayant@gmail.com>
2023-02-06 19:46:31 +0530

    day-2

---
+++ Day-2/Day(2)_jayant.py
@@ -0,0 +1,15 @@
+First_name="Jayant"
+Last_name="Agarwal"
+Country="India"
+City="Bhubneshwar"
+Age=19
+DOB="16/06/2003"
+is_true=First_name*2
+print(is_true)
+
+print(type(First_name))
+print(type(Last_name))
+print(type(Country))
+print(type(City))
+print(type(Age))
+print(type(DOB))
