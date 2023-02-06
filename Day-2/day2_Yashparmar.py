d1e6dc8d911072c9a97f69214d87606a2183a7d6
Yash Parmar <86340003+Yash1073@users.noreply.github.com>
2023-02-06 21:16:20 +0530

    Day 2 Challange

---
+++ Day-2/day2_Yashparmar.py
@@ -0,0 +1,12 @@
+first_name='Yash'
+last_name='Parmar'
+country='India'
+city='Bhavnagar'
+age=18
+dob='10/07/2003'
+print("Firstname datatype is \n",type(first_name))
+print("Lastname datatype is \n",type(last_name))
+print("Country datatype is \n",type(country))
+print("City datatype is \n",type(city))
+print("Age data type is \n",type(age))
+print("Datheof birth data type is \n",type(dob))
