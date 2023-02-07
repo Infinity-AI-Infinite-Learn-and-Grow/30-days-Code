8701c21764d1c020f5a2c1a137ca1f9054b84c86
jayant-01 <01agarwal.jayant@gmail.com>
2023-02-07 17:26:48 +0530

    day-3

---
+++ Day-3/Day(3)_jayant.py
@@ -0,0 +1,15 @@
+#calculating area of a triangle
+b=float(input("Enter the base of the triangle: "))
+h=float(input("Enter the height of the triangle: "))
+area=0.5*b*h
+print("The area of the triangle is: ",area)
+
+# calculating area of a circle
+r=float(input("Enter the radius of the circle: "))
+area=3.14*r*r
+print("The area of the circle is: ",area)
+
+# calculating circumference of the circle
+circumference=2*3.14*r
+print("The circumference of the circle is: ",circumference)
+
