5f70b5542c58c4192e2ce7f2e43b1165db2104ce
hetsonii <21cs068@charusat.edu.in>
2023-02-08 22:15:52 +0530

    Day4

---
+++ Day-4/Day(4)_hetsonii.py
@@ -0,0 +1,16 @@
+part1 = "Thirty"
+part2 = "Days"
+part3 = "Of"
+part4 = "Code"
+
+task1 = " ".join([part1, part2, part3, part4])
+print("Concatenated string:\t", task1)
+
+task2 = "Ai For All"
+print("\nCapitalized: \t", task2.capitalize())
+print("Titled: \t", task2.title())
+print("Case-swaped: \t", task2.swapcase())
+
+task3 = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+final = task3.split(", ")
+print("\nSplited: ", final)
