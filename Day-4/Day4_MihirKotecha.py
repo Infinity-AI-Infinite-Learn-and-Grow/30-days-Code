9ff47ac28a8cabaf3b7b35359226f87421cbfbbb
MihirKotecha <mihir.k1@ahduni.edu.in>
2023-02-08 15:15:58 +0530

    Day-4

---
+++ Day-4/Day4_MihirKotecha.py
@@ -0,0 +1,19 @@
+s1 = 'Thirty'
+s2 = 'Days'
+s3 = 'Of'
+s4 = 'Code'
+
+s = s1 + ' ' + s2 + ' ' + s3 + ' ' + s4
+
+print(s)
+
+m = 'Ai'
+
+print(m.capitalize())
+print(m.title())
+print(m.swapcase())
+
+txt = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+print(txt.split(','))
+
