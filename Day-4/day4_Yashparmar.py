ca1b06ca1dee2d354fa7a74385458a8098baf76e
Yash Parmar <86340003+Yash1073@users.noreply.github.com>
2023-02-08 18:50:09 +0530

    Day 4 Challange

---
+++ Day-4/day4_Yashparmar.py
@@ -0,0 +1,24 @@
+    a='Thirty'
+    b='Days'
+    c='Of'
+    d='Code'
+    print("Concate String is {}".format(a+" " +b+" "+c+" "+d))
+    
+    #capitalize():- return the string with first letter uppper case and rest are in lower case
+    str="Thirty Days Of Code"
+    print("Using Capitalize function ",str.capitalize())
+    
+    #title():- return the string where first character in every word is upper case
+    str="Infinity AI thirty DAYS OF cOde"
+    print("Using title function ",str.title())
+    
+    #swapcase():- returns a string where all the upper case letters are lower case 
+    str="Infinity AI thirty DAYS OF cOde"
+    print("Using swapcase function ",str.swapcase())
+    
+    #Split the string at the comma
+    str='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+    a=str.split(',')
+    print(a)
+            
+    
