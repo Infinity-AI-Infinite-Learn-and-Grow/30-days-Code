ab316169bd4e65169b78bc22ff2b9ae5618448d1
mahekkg <mahekg2001@gmail.com>
2023-02-21 22:19:00 +0530

    Day17 file added

--- Day-14/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-Sweden', 'Denmark', 'Norway', 'Iceland']
-# names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
-
-# cap_country=map(countries.capitalize(),countries)
---
+++ Day-17/Day(17)_MahekGoswami.py
@@ -0,0 +1,8 @@
+#Day 17
+
+#-------Task 1-------
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+nordic_countries=names
+Finland, Sweden, Norway,Denmark,Iceland,es,ru = names
+
+#-------Task 2-------
