8c8b020b1ff00f24808a6e410b7d167820a8e6af
itsabdullah15 <itsabdullah.cg@gmail.com>
2023-02-24 13:52:39 +0530

    Day(15)_Abdullah

---
+++ Day-15/Day(15)_abdullah.py
@@ -0,0 +1,35 @@
+# Open your python interactive shell and try all the types of errors like syntax error,
+# NameError, IndexError, ModuleNotFoundError, AttributeError, KeyError, TypeError,
+# ImportError, ValueError, and ZeroDivisionError.
+# name error
+import math
+printf("hello")
+
+# syntax error
+for i in range(5)
+print(i)
+
+# type error
+print(5+"5")
+
+# value error
+int("hello")
+
+# zero division error
+print(5/0)
+
+# index error
+a = [1, 2, 3]
+print(a[5])
+
+#import error
+
+# key error
+a = {1: 2, 3: 4}
+print(a[5])
+
+# attribute error
+a = 5
+print(a.append(5))
+
+# module not found error
