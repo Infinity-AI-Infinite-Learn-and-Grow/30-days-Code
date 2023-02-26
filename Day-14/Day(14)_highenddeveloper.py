dae8ff172bda34e4588722dc209b045e3902b01f
Shubham bansiwal <68029830+highenddeveloper@users.noreply.github.com>
2023-02-26 22:09:32 +0530

    day 14

---
+++ Day-14/Day(14)_highenddeveloper.py
@@ -0,0 +1,24 @@
+#Open your python interactive shell and try all the types of errors like syntax error, NameError, IndexError, ModuleNotFoundError, AttributeError, KeyError, TypeError, ImportError, ValueError, and ZeroDivisionError.
+
+# Syntax Error
+print("Hello World")
+# NameError
+print(hello)
+# IndexError
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+print(countries[6])
+# ModuleNotFoundError
+import math
+# AttributeError
+import math
+print(math.pi)
+# KeyError
+
+# TypeError
+print(5 + '5')
+# ImportError
+import math
+# ValueError
+print(int('5.5'))
+# ZeroDivisionError
+print(5/0)
